ce0e861dc45f813e6e29d49af784f5e2933b0958
lzq <zhiqiang.liu@aihuishou.com>
2020-12-02 22:55:37 +0800

    add Decorator Patterns

---
+++ src/main/java/structure/decorator/DataSourceDecorator.java
@@ -0,0 +1,22 @@
+package structure.decorator;
+
+/**
+ * 抽象基础类
+ */
+public abstract class DataSourceDecorator implements DataSource {
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
